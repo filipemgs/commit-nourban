c57b641a9b5edd135a620b8c35d879d1662743a5
Rui Ribeiro <rui.ribeiro@jscrambler.com>
2018-08-02 09:52:30 +0100

    Add line 4

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ const fs = require('fs');
 const shell = require('shelljs');
 
 const stage = (file) => shell.exec(`git add ${file}`).code;
+// Get git's working tree status and returns list of new or modified files
