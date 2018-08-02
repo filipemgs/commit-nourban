5c6f35801e9e8b83f38d0ed9f291a2e002ce30e8
Rui Ribeiro <rui.ribeiro@jscrambler.com>
2018-08-02 09:52:30 +0100

    Add line 7

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ const stage = (file) => shell.exec(`git add ${file}`).code;
 // Get git's working tree status and returns list of new or modified files
 const treeStatus = () => 
   shell.exec('git status --short')
+  .stdout
